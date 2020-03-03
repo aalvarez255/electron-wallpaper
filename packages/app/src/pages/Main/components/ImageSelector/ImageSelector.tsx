3a2c1183041ef5194df025c52d78304d5529ed10
Adrian Alvarez <aalvarez255@gmail.com>
2020-03-03 19:35:41 +0100

    interval

--- packages/app/src/pages/Main/components/ImageSelector/ImageSelector.tsx
+++ packages/app/src/pages/Main/components/ImageSelector/ImageSelector.tsx
@@ -2,59 +2,85 @@ import React, { Component, ChangeEvent } from "react"
 import './ImageSelector.scss'
 
 export default class ImageSelector extends Component {
-  fileInput: HTMLInputElement | null = null
+    fileInput: HTMLInputElement | null = null
 
-  state = {
-    images: []
-  }
-
-  render() {
-    return (
-      <div className='selector'>
-        <div>
-          <label>1. Choose some images from disk:</label>
-        </div>
-        <div>
-          <input
-            ref={fileInput => this.fileInput = fileInput}
-            type='file'
-            accept="image/x-png,image/gif,image/jpeg"
-            multiple
-            onChange={this.onImageSelected} />
-
-          <div className='btn btn-one' onClick={this.triggerInputFile}>
-            <span>SELECT</span>
-          </div>
-
-          <div className='images'>
-            {
-              this.state.images.map(x =>
-                <div className='image'>
-                  <img src={x} alt='img' />
-                </div>                
-              )
-            }
-          </div>
-        </div>
-      </div>
-    )
-  }
-
-  onImageSelected = (event: ChangeEvent<HTMLInputElement>) => {
-    const files = event.currentTarget.files as any
-    this.setState({
-      images: []
-    })
-
-    for (let file of files) {
-      const reader = new FileReader()
-      reader.readAsDataURL(file)
-      reader.onloadend = _ =>  this.setState({
-        images: [...this.state.images, reader.result]
-      })
+    interval: number = 0
+    state = {
+        playing: false,
+        images: [],
+        selectedIndex: null
     }
-  }
 
-  triggerInputFile = () => this.fileInput?.click()
+    render() {
+        return (
+            <div className='selector'>
+                <div>
+                    <label>Choose some images from disk:</label>
+                </div>
+                <div>
+                    <input
+                        ref={fileInput => this.fileInput = fileInput}
+                        type='file'
+                        accept="image/x-png,image/gif,image/jpeg"
+                        multiple
+                        onChange={this.onImageSelected} />
 
+                    <div className='btn btn-one' onClick={this.triggerInputFile}>
+                        <span>SELECT</span>
+                    </div>
+
+                    <div className='images'>
+                        {
+                            this.state.images.map(x =>
+                                <div className='image' style={{
+                                    backgroundImage: "url(" + x + ")",
+                                }}>
+                                </div>
+                            )
+                        }
+                    </div>
+                </div>
+                {
+                    this.state.images.length ?
+                        <div className={'btn btn-two ' + (this.state.playing ? 'active' : '')} onClick={this.toggleWallpapers}>
+                            <span>{this.state.playing ? 'STOP' : 'PLAY'}</span>
+                        </div> : null
+                }
+            </div>
+        )
+    }
+
+    onImageSelected = (event: ChangeEvent<HTMLInputElement>) => {
+        const files = event.currentTarget.files as any
+        this.setState({
+            images: []
+        })
+
+        for (let file of files) {
+            const reader = new FileReader()
+            reader.readAsDataURL(file)
+            reader.onloadend = _ => this.setState({
+                images: [...this.state.images, reader.result]
+            })
+        }
+    }
+
+    triggerInputFile = () => this.fileInput?.click()
+    toggleWallpapers = () => {
+        this.setState((state: any) => ({
+            playing: !state.playing
+        }), () => {
+            if (this.state.playing) {
+                this.interval = window.setInterval(() => this.setWallpaper(), 3000)
+            }
+            else {
+                clearInterval(this.interval)
+            }
+        })
+
+    }
+
+    setWallpaper = () => {
+        console.log('ss')
+    }
 }
